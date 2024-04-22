62b15eee2430f68a3e0b703001f6a08ad428b230
Zhongly <632283391@qq.com>
2024-04-22 17:50:52 +0800

    initUpdate

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,30 @@
 import typescript from '@rollup/plugin-typescript';
 import json from '@rollup/plugin-json';
+import { terser } from 'rollup-plugin-terser';
+// const license = require('rollup-plugin-license');
+import license from 'rollup-plugin-license';
+
+// const { banner } = pkg
+// const license = `
+// /**
+//  * @Rabbitor/radix
+//  * (c) 2024 Rabbitor
+//  * Released under the MIT License.
+//  */
+// `
 export default {
-  input: 'src/index.ts',
+  input: 'index.ts',
   output: {
     dir: 'dist',
     format: 'cjs'
   },
-  plugins: [typescript(), json()]
+  plugins: [json(), typescript(), terser(), license({
+    banner: {
+      content: `
+        MIT License
+        Author: Rabbitor
+        @Rabbitor/radix v1.0.0
+      `
+    }
+  })]
 }
